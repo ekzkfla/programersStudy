663b040e6dab828d4326cc8a80b0aa13bd931cb2
Harim <godtoether94@naver.com>
2022-12-15 22:14:10 +0900

    2022.12.15 짝수싫어요 문제

---
+++ src/level0/짝수는싫어요.java
@@ -0,0 +1,15 @@
+package level0;
+
+public class 짝수는싫어요 {
+	
+	public int[]solution(int n){
+		int[] answer= {};
+		for(int i=1; i<=100; i++) {
+			if(i%n==1) {
+				answer=
+			}
+		}
+		
+		return answer;
+	}
+}
