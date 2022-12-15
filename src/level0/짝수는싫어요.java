20bf9e083db3364576465d364955463fe6a1c164
Harim <godtoether94@naver.com>
2022-12-15 22:21:47 +0900

    2022.12.15 짝수는 싫어요 문제

--- src/level0/짝수는싫어요.java
+++ src/level0/짝수는싫어요.java
@@ -1,15 +1,15 @@
 package level0;
 
+
 public class 짝수는싫어요 {
 	
 	public int[]solution(int n){
-		int[] answer= {};
-		for(int i=1; i<=100; i++) {
-			if(i%n==1) {
-				answer=
+		int[] answer= new int[(n+1)/2];
+		for(int i=1; i<=n; i++) {
+			if(i%2==1) {
+				answer[i/2]=i;
 			}
 		}
-		
 		return answer;
 	}
 }
